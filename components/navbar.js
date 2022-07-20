d6c20c18012dc63855de1637bea70af9a2ce05d9
sandeepprogrammer64 <mail4sandeep44@gmail.com>
2022-07-20 14:38:46 +0530

    added navbar part

---
+++ components/navbar.js
@@ -0,0 +1,20 @@
+
+let  navbar = () => 
+{
+
+return  `
+    
+            
+    <a href="https://www.airgarage.com/">
+        <img src="https://assets.website-files.com/5d55f1425cb6b7a18aa77528/5d55f8a49a649a59aab64b1c_airgarage-logo.png" alt="">
+    </a>
+    <a href="#"> 
+        <p id="link1">Find parking</p> </a>
+    <a id="link2" href="#">Log in</a>
+    <a id= "link3" href="#">Talk to Sales</a>
+    `
+
+    
+}
+
+export default navbar;
