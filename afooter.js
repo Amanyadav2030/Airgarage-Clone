758cf00997e983e5f55562f6bb5e08e2ccd72724
kavita kharat <sawadekavita.1102@gmail.com>
2022-07-22 19:56:30 +0530

    day3 added

--- afooter.js
+++
@@ -1,9 +0,0 @@
-function footer(){
-    return `<p>©2021 AirGarage, Inc</p>
-    <ul>
-      <li><a href="#">Homepage</a></li>
-      <li><a href="#">Terms</a></li>
-      <li><a href="#">Contact us</a></li>
-    </ul>`
-};
-export {footer};
