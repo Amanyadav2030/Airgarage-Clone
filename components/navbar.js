7ad70fb8d0868fe121633c2e24ab0e28d404a4ca
Amanyadav2030 <Amanyadav919821@gmail.com>
2022-07-23 09:56:04 +0530

    day 4 tasks

--- components/navbar.js
+++ components/navbar.js
@@ -5,7 +5,7 @@ let  navbar = () =>
 return  `
     
             
-    <a href="https://www.airgarage.com/">
+    <a href="index.html">
         <img src="https://assets.website-files.com/5d55f1425cb6b7a18aa77528/5d55f8a49a649a59aab64b1c_airgarage-logo.png" alt="">
     </a>
     <a href="#"> 
