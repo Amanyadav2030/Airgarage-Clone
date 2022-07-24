9ee9962cd2726cf2b65b31db6af25556d4c1e365
A_man Yadav <103621772+Amanyadav2030@users.noreply.github.com>
2022-07-24 13:20:21 +0530

    Update signup.js

--- channascripts/signup.js
+++ channascripts/signup.js
@@ -1,4 +1,4 @@
-document.querySelector("#btn").addEventListener("click",()=>{
+document.querySelector("#but").addEventListener("click",()=>{
     setTimeout(() => {
         window.location.href="login.html";
     }, 2000);
